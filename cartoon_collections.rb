fd8892a54fc2dc4c5f4002d2ba0a09c46f097894
Milan Parmar <milan_parmar@hotmail.co.uk>
2019-12-16 19:27:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -27,7 +27,7 @@ def find_the_cheese(array)
   
   cheese_types = ["cheddar", "gouda", "camembert"]
    
-    array.detect {|i| i.type}
+    array.detect {|type| type.include?}
 
   
   #the array below is here to help
